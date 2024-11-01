004793579547da1a922d2a876cfafd0f2785974d
Michael Neill Hartman <16545449+hartmanm@users.noreply.github.com>
2024-11-01 13:31:25 +0000

    changed to also check in bin usr/bin

--- mnh.c
+++ mnh.c
@@ -12,33 +12,32 @@
 #include <dirent.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <sys/stat.h>
 
-int main(int number_of_parameters,char* parameters[]){
+int check_executable(const char *path,const char *exec_name){
+struct stat statbuf;
+char exec_path[512];
+snprintf(exec_path,sizeof(exec_path),"%s/%s",path,exec_name);
+return(stat(exec_path,&statbuf)==0&&(statbuf.st_mode&S_IXUSR));
+}
+int main(int number_of_parameters,char *parameters[]){
 if(number_of_parameters<2){
 fprintf(stderr,"Usage: %s <target_executable> [<additional_parameters>...]\n",parameters[0]);
 return EXIT_FAILURE;
 }
-char* exec_name=parameters[1];
+char *exec_name=parameters[1];
 char exec_path[512];
+if(check_executable("wrap",exec_name)){
 snprintf(exec_path,sizeof(exec_path),"wrap/%s",exec_name);
-DIR* dir=opendir("wrap");
-if(!dir){
-perror("failed to open wrap");
+}else if(check_executable("/bin",exec_name)){
+snprintf(exec_path,sizeof(exec_path),"/bin/%s",exec_name);
+}else if(check_executable("/usr/bin",exec_name)){
+snprintf(exec_path,sizeof(exec_path),"/usr/bin/%s",exec_name);
+}else{
+fprintf(stderr,"target executable '%s' not found in 'wrap', '/bin', or '/usr/bin'\n",exec_name);
 return EXIT_FAILURE;
 }
-struct dirent* entry;
-int found=0;
-while((entry=readdir(dir))!=NULL){
-if(strcmp(entry->d_name,exec_name)==0){
-found=1;
-break;
-}}
-closedir(dir);
-if(!found){
-fprintf(stderr,"target executable '%s' not found\n",exec_name);
-return EXIT_FAILURE;
-}
-char* command[number_of_parameters];
+char *command[number_of_parameters];
 command[0]=exec_path;
 for(int i=1;i<number_of_parameters;i++){
 command[i]=parameters[i];
